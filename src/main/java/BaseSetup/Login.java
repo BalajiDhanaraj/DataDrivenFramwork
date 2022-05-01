741fce12dc39fdbc484cc1aca700e4139b6d92c2
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-05-01 14:59:36 +0530

    upadated

--- src/main/java/BaseSetup/Login.java
+++ src/main/java/BaseSetup/Login.java
@@ -3,24 +3,34 @@ package BaseSetup;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeUnit;
 
 public class Login {
 
     public static WebDriver driver;
-    @Test
-    public void Login(){
 
+    public String[][] data = {
+            {"Admin1","admin"},
+            {"Admin1","admin2"},
+            {"Admin1","admin3"}
+    };
+
+    @DataProvider(name = "loginData")
+    public Object[][] LoginDataProvider(){
+        return data;
+    }
+
+    @Test(dataProvider ="loginData")
+    public void Login(String uname,String pass){
         System.setProperty("webdriver.chrome.driver","/Volumes/Macintosh HD/For Mac/java text and soft file and java IDE/Intellij java project/Drivers/chromedriver");
 
          driver = new ChromeDriver();
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
 
-        driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys("nn");
+        driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys(uname);
 
-         driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys("jkj");
+         driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(pass);
     }
 
 
