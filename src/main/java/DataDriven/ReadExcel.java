7c6c1748d93deca4d6ff03f17765fc1aa0aa5760
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-05-02 09:42:43 +0530

    upadated

---
+++ src/main/java/DataDriven/ReadExcel.java
@@ -0,0 +1,38 @@
+package DataDriven;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ReadExcel {
+
+        public static WebDriver driver;
+
+        public String[][] data = {
+                {"Admin1","admin"},
+                {"Admin1","admin2"},
+                {"Admin1","admin3"}
+        };
+
+        @DataProvider(name = "loginData")
+        public Object[][] LoginDataProvider(){
+            return data;
+        }
+
+        @Test(dataProvider ="loginData")
+        public void Login(String uname,String pass){
+            System.setProperty("webdriver.chrome.driver","/Volumes/Macintosh HD/For Mac/java text and soft file and java IDE/Intellij java project/Drivers/chromedriver");
+
+            driver = new ChromeDriver();
+            driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+
+            driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys(uname);
+
+            driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(pass);
+        }
+
+
+    }
+
